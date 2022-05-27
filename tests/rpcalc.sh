6ce75b312c5f930e13055523e3465f9169251a66
Mike Fulton <mikefultonpersonal@gmail.com>
2022-05-27 00:17:03 -0400

    add testcase showing basic bison failure

---
+++ tests/rpcalc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Compile and run the rpcalc example that comes with bison
+# This presumes you have 'make' available in boot and 'bison' in prod (from make install)
+#
+set -x
+
+progdir=$( cd $( dirname $0 ); echo $PWD )
+
+cd ${HOME}/zot/prod/bison || exit 99
+. ./.env
+cd ${HOME}/zot/boot/make || exit 99
+. ./.env
+
+cd "${progdir}"
+rpcalcdir=$(find ../ -name rpcalc -type d | head -1)
+
+cd ${rpcalcdir} || exit 99
+
+echo "4 9 +" | make run
+
+
