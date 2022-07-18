cce649debb0a7f768785824bc242a9f1a22ffd5c
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:46:50 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'bisonport'), string(name: 'DESCRIPTION', 'bisonport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'bisonport'), string(name: 'DESCRIPTION', 'Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser.' )]
         }
 }
