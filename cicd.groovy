b7236d59382da7fd76e7350ad2032cb7489fa7f2
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:28 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/bisonport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/bisonport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/bisonport.git'), string(name: 'PORT_DESCRIPTION', value: 'Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser.' ), string(name: 'NODE_LABEL', value: "v2r4")]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/bisonport.git'), string(name: 'PORT_DESCRIPTION', value: 'Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser.' ), string(name: 'NODE_LABEL', value: "v2r4")]
         }
 }
